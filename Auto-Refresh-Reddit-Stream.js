cc1ce1daaa66f74dd5f7780db12298483f71fbaf
Typpi <20943337+Nick2bad4u@users.noreply.github.com>
2024-05-02 23:33:54 -0400

    Create Auto-Refresh-Reddit-Stream.js

---
+++ Auto-Refresh-Reddit-Stream.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      2024-05-03
+// @description  try to take over the world!
+// @author       You
+// @match        https://reddit-stream.com/comments/1ciurrx/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit-stream.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    setTimeout(function(){ location.reload(); }, 30*1000);
+})();
