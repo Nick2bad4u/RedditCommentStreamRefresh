684f6f2c2dfe449a4c063a7aa6a638dcb3fe4a7f
Typpi <20943337+Nick2bad4u@users.noreply.github.com>
2024-05-03 00:09:50 -0400

    Update Auto-Refresh-Reddit-Stream.js

--- Auto-Refresh-Reddit-Stream.js
+++ Auto-Refresh-Reddit-Stream.js
@@ -2,16 +2,17 @@
 // @name         Reddit-Stream Auto-Refresh 
 // @namespace    nick2bad4u
 // @description  Auto-Refreshes your Reddit-Stream.com page every 30 seconds. Modify the script and change to any time you want.
-// @author       https://github.com/Nick2bad4u
+// @author       Nick2bad4u
 // @match        https://reddit-stream.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit-stream.com
-// @version      1.2
+// @version      1.3
 // @grant        none
 // @homepage     https://github.com/Nick2bad4u/RedditCommentStreamRefresh
 // @homepageURL  https://github.com/Nick2bad4u/RedditCommentStreamRefresh
 // @supportURL   https://github.com/Nick2bad4u/RedditCommentStreamRefresh/issues
 // @downloadURL  https://raw.githubusercontent.com/Nick2bad4u/RedditCommentStreamRefresh/main/Auto-Refresh-Reddit-Stream.js
 // @updateURL    https://raw.githubusercontent.com/Nick2bad4u/RedditCommentStreamRefresh/main/Auto-Refresh-Reddit-Stream.js
+// @license      The Unlicense; https://unlicense.org/
 // ==/UserScript==
 
 (function() {
