e0c3e71b5775be59eb0c5391fc61a4760c59e069
Typpi <20943337+Nick2bad4u@users.noreply.github.com>
2024-05-02 23:43:20 -0400

    Update Auto-Refresh-Reddit-Stream.js

--- Auto-Refresh-Reddit-Stream.js
+++ Auto-Refresh-Reddit-Stream.js
@@ -1,15 +1,17 @@
 // ==UserScript==
-// @name         New Userscript
+// @name        30 Second Auto-Refresh Reddit-stream.com
 // @namespace    http://tampermonkey.net/
 // @version      2024-05-03
-// @description  try to take over the world!
-// @author       You
-// @match        https://reddit-stream.com/comments/1ciurrx/
+// @description  Auto-Refreshes your Reddit-Stream.com page every 30 seconds. Modify the script and change to any time you want.
+// @author       https://github.com/Nick2bad4u
+// @match        https://reddit-stream.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit-stream.com
 // @grant        none
+// @downloadURL https://raw.githubusercontent.com/Nick2bad4u/RedditCommentStreamRefresh/main/Auto-Refresh-Reddit-Stream.js
+// @updateURL https://raw.githubusercontent.com/Nick2bad4u/RedditCommentStreamRefresh/main/Auto-Refresh-Reddit-Stream.js
 // ==/UserScript==
 
 (function() {
     'use strict';
-    setTimeout(function(){ location.reload(); }, 45*1000);
+    setTimeout(function(){ location.reload(); }, 30*1000);
 })();
