e5dd4bce6a5cd7c1b0da49dcc8177aca635ecbf4
Typpi <20943337+Nick2bad4u@users.noreply.github.com>
2024-05-04 15:04:28 -0400

    Update Auto-Refresh-Reddit-Stream.meta.js

--- Auto-Refresh-Reddit-Stream.meta.js
+++ Auto-Refresh-Reddit-Stream.meta.js
@@ -11,7 +11,7 @@
 // @homepageURL  https://github.com/Nick2bad4u/RedditCommentStreamRefresh
 // @supportURL   https://github.com/Nick2bad4u/RedditCommentStreamRefresh/issues
 // @downloadURL  https://raw.githubusercontent.com/Nick2bad4u/RedditCommentStreamRefresh/main/Auto-Refresh-Reddit-Stream.js
-// @updateURL    https://raw.githubusercontent.com/Nick2bad4u/RedditCommentStreamRefresh/main/Auto-Refresh-Reddit-Stream.js
+// @updateURL    https://raw.githubusercontent.com/Nick2bad4u/RedditCommentStreamRefresh/main/Auto-Refresh-Reddit-Stream.meta.js
 // @license CC-BY-SA-3.0; http://creativecommons.org/licenses/by-sa/3.0/
 // @license MIT
 // ==/UserScript==
